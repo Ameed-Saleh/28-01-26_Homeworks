4828283a0d37fa05c1786c9d8431d7cdfed03d1e
Ameed-Saleh <ameed.saleh@outlook.com>
2026-02-04 00:22:12 +0200

    Four Tasks!

--- 28-01-26_Tasks/Q-4-Bonus_רצף עולה של 3 מספרים.py
+++ 28-01-26_Tasks/Q-4-Bonus_רצף עולה של 3 מספרים.py
@@ -1,13 +1,14 @@
 _count = 1
-_previous = int(input(" הכנס מספר ?"))
+_previous = None
 while True:
     _current = int(input("הכנס מספר נוכחי:"))
-    if _current > _previous:
-        _count += 1
+    if _previous is None:
         _previous = _current
+    if _current > _previous:
+       _count += 1
     else:
         _count = 1
-        continue
+    _previous = _current
     if _count == 3:
         break
 print(" התקבל רצף של 3 מספרים עוקבים עולים,התוכנית הסתיימה!")
